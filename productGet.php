128eebf5d75c2c5ff389e1e850c352641e463755
SunaliB <147754754+Basnayake07@users.noreply.github.com>
2024-08-28 19:35:49 +0530

    Update productGet.php

--- productGet.php
+++ productGet.php
@@ -112,25 +112,7 @@ $role = $_SESSION['role'];
                     </thead>
                     <tbody>
                     <?php
-                            $host = 'camerastore.mysql.database.azure.com';
-                            $port = 3306;
-                            $username = 'camerastore';
-                            $password = 'ognam@#123';
-                            $dbname = 'Camera_Warehouse';
-                            
-                            // Path to your SSL certificate
-                            $ssl_ca = '/home/site/wwwroot/ca-cert.pem'; // Ensure this path is correct
-                            
-                            // Create connection with SSL
-                            $mysqli = new mysqli();
-                            $mysqli->ssl_set(null, null, $ssl_ca, null, null);
-                            $mysqli->real_connect($host, $username, $password, $dbname, $port, null, MYSQLI_CLIENT_SSL);
-                            
-                            // Check connection
-                            if ($mysqli->connect_error) {
-                                echo json_encode(['status' => 'error', 'message' => 'Database connection failed']);
-                                exit();
-                            }
+                            include "config.php";
 
                             $sql = "SELECT * FROM products";
                             $result = $mysqli->query($sql);
