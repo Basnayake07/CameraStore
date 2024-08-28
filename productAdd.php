bad1685dc91b219d0b1bc6920f339dd51ddf7246
SunaliB <147754754+Basnayake07@users.noreply.github.com>
2024-08-28 20:42:35 +0530

    Update productAdd.php

--- productAdd.php
+++ productAdd.php
@@ -1,35 +1,18 @@
 <?php
 
-$host = 'camerastore.mysql.database.azure.com';
-$port = 3306;
-$username = 'camerastore';
-$password = 'ognam@#123';
-$dbname = 'Camera_Warehouse';
-
-// Path to your SSL certificate
-$ssl_ca = '/home/site/wwwroot/ca-cert.pem'; // Ensure this path is correct
-
-// Create connection with SSL
-$mysqli = new mysqli();
-$mysqli->ssl_set(null, null, $ssl_ca, null, null);
-$mysqli->real_connect($host, $username, $password, $dbname, $port, null, MYSQLI_CLIENT_SSL);
-
-// Check connection
-if ($mysqli->connect_error) {
-    echo json_encode(['status' => 'error', 'message' => 'Database connection failed']);
-    exit();
-}
+include "config.php";
 
 if (isset($_POST['submit'])) {
     $productName = $_POST['productname'];
     $brand = $_POST['brand'];
     $type = $_POST['type'];
     $sku = $_POST['sku'];
-        
-    $stmt = $mysqli->prepare("INSERT INTO products (ProductName, Brand, Type, SKU) VALUES (?, ?, ?, ?)");
+    $dateAdded = $_POST['dateadded'];
+
+    $stmt = $mysqli->prepare("INSERT INTO products (ProductName, Brand, Type, SKU, DateAdded) VALUES (?, ?, ?, ?, ?)");
     
     if ($stmt) {
-        $stmt->bind_param("ssssss", $productName, $brand, $type, $sku);
+        $stmt->bind_param("sssss", $productName, $brand, $type, $sku, $dateAdded);
         
         if ($stmt->execute()) {
             header("Location: productGet.php?msg=New record created successfully");
@@ -40,10 +23,9 @@ if (isset($_POST['submit'])) {
 
         $stmt->close();
     } else {
-        echo "Failed to prepare the SQL statement: " . $mysqli->error;
+        echo "Failed to prepare the SQL statement: " . $conn->error;
     }
 }
 
 $mysqli->close();
 ?>
-
