d054b2bea462bbfa5efc90fbefbc1e4726c4a81a
SunaliB <147754754+Basnayake07@users.noreply.github.com>
2024-08-29 10:15:52 +0530

    Update purchaseView.php

--- purchaseView.php
+++ purchaseView.php
@@ -127,7 +127,25 @@ $role = $_SESSION['role'];
 
                     
                     // Include config file
-                    require_once "config.php";
+                    $host = 'camerastore.mysql.database.azure.com';
+                    $port = 3306;
+                    $username = 'camerastore';
+                    $password = 'ognam@#123';
+                    $dbname = 'Camera_Warehouse';
+                    
+                    // Path to your SSL certificate
+                    $ssl_ca = '/home/site/wwwroot/ca-cert.pem'; // Ensure this path is correct
+                    
+                    // Create connection with SSL
+                    $mysqli = new mysqli();
+                    $mysqli->ssl_set(null, null, $ssl_ca, null, null);
+                    $mysqli->real_connect($host, $username, $password, $dbname, $port, null, MYSQLI_CLIENT_SSL);
+                    
+                    // Check connection
+                    if ($mysqli->connect_error) {
+                        echo json_encode(['status' => 'error', 'message' => 'Database connection failed']);
+                        exit();
+                    }
 
                     // Attempt select query execution
                     $sql = "SELECT ps.PurchaseOrderID, ps.QuantityOrdered, ps.QuantityRecieved, ps.UnitPrice, ps.OrderDate, ps.Status, p.ProductName, s.SupplierName FROM products p, purchaseorders ps, suppliers s WHERE ps.ProductID = p.ProductID AND ps.SupplierID = s.SupplierID ORDER BY ps.PurchaseOrderID ;";
