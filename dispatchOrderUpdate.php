ca3cb14a1355c775cf07208f728642113a41908e
SunaliB <147754754+Basnayake07@users.noreply.github.com>
2024-08-29 13:46:44 +0530

    Update dispatchOrderUpdate.php

--- dispatchOrderUpdate.php
+++ dispatchOrderUpdate.php
@@ -3,7 +3,26 @@
 //die("Error executing query: " . mysqli_error($mysqli));
 
 //mysqliect to the database
-include 'config.php';
+$host = 'camerastore.mysql.database.azure.com';
+$port = 3306;
+$username = 'camerastore';
+$password = 'ognam@#123';
+$dbname = 'Camera_Warehouse';
+
+// Path to your SSL certificate
+$ssl_ca = '/home/site/wwwroot/ca-cert.pem'; // Ensure this path is correct
+
+// Create connection with SSL
+$mysqli = new mysqli();
+$mysqli->ssl_set(null, null, $ssl_ca, null, null);
+$mysqli->real_connect($host, $username, $password, $dbname, $port, null, MYSQLI_CLIENT_SSL);
+
+// Check connection
+if ($mysqli->connect_error) {
+    echo json_encode(['status' => 'error', 'message' => 'Database connection failed']);
+    exit();
+}
+
 session_start();
 // Check if the user is logged in, if not redirect to login page
 if (!isset($_SESSION['username'])) {
